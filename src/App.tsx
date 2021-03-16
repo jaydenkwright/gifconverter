d1266fe42639c0e7698e5def53fbae4b814451d5
Jayden Wright <jaydenkwright3@gmail.com>
2021-03-16 19:01:42 -0400

    Moved Gif, Video, Upload button and convert button to seperate components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState, useEffect, useRef } from 'react'
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
+import { useState, useEffect } from 'react'
+import { createFFmpeg } from '@ffmpeg/ffmpeg'
+import Video from './components/Video'
+import Gif from './components/Gif'
+import UploadButton from './components/UploadButton'
+import ConvertButton from './components/ConvertButton'
 
 const ffmpeg = createFFmpeg({ log: true })
 function App() {
   const [ready, setReady] = useState<boolean>(false)
-  const [video, setvideo] = useState<File | null | undefined>()
+  const [video, setVideo] = useState<File | null | undefined>()
   const [gif, setGif] = useState<any>()
   const [convertLoading, setConvertLoading] = useState<boolean>(false)
 
@@ -17,41 +21,26 @@ function App() {
     }
   }
 
-  const convert = async () => {
-    if (video){
-      setConvertLoading(true)
-      ffmpeg.FS('writeFile', video.name, await fetchFile(video))
-      await ffmpeg.run('-i', video.name, '-t', '30', '-f', 'gif', 'gif.gif')
-      const convertedGif = ffmpeg.FS<any>('readFile', 'gif.gif')
-      const gifUrl = URL.createObjectURL(new Blob([convertedGif.buffer], {type: 'image/gif'}))
-      setGif(gifUrl)
-      setConvertLoading(false)
-    }
-  }
-
   useEffect(() => {
     load()
   }, [])
 
-  const hiddenFileInput = useRef<any>(null);
-
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    hiddenFileInput?.current?.click();
-  };
-
   return ready ? (
     <div className="container">
-      {video && !gif && <video 
-        controls 
-        width='400' 
-        src={URL.createObjectURL(video)} 
-      />
-      }
+      {video && !gif && <Video src={URL.createObjectURL(video)} /> }
       {convertLoading && 'Converting...'}
-      {gif && <img src={gif} width='400' alt='Gif'/>}
-      {video && !gif && <button onClick={convert} className='convertButton'>Convert</button>}
-      <input type='file' ref={hiddenFileInput} onChange={(e) => setvideo(e.target.files?.item(0))} accept='video/*' style={{display: 'none'}}/>
-      {!video || gif ? <button className='uploadButton' onClick={handleClick} >Select Video File</button> : null}
+      {gif && <Gif src={gif} />}
+      {video && !gif && 
+      <ConvertButton 
+        video={video} 
+        setConvertLoading={setConvertLoading} 
+        setGif={setGif} 
+        ffmpeg={ffmpeg}
+      />}
+      <UploadButton 
+        setVideo={setVideo} 
+        video={video} 
+      />
     </div>
   ): <p>Loading...</p>;
 }
