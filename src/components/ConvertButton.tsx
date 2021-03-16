323c05fc571bd8165a48db195b3bca045ef38550
Jayden Wright <jaydenkwright3@gmail.com>
2021-03-16 19:00:22 -0400

    Moved convert button and its logic to seperate component

---
+++ src/components/ConvertButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { fetchFile } from '@ffmpeg/ffmpeg'
+
+interface Props{
+    video: File | null | undefined
+    setConvertLoading: any
+    setGif: any
+    ffmpeg: any
+}
+
+const ConvertButton: React.FC<Props> = ({ video, setConvertLoading, setGif, ffmpeg }) => {
+    const convert = async () => {
+        if (video){
+          setConvertLoading(true)
+          ffmpeg.FS('writeFile', video.name, await fetchFile(video))
+          await ffmpeg.run('-i', video.name, '-t', '30', '-f', 'gif', 'gif.gif')
+          const convertedGif = ffmpeg.FS('readFile', 'gif.gif')
+          const gifUrl = URL.createObjectURL(new Blob([convertedGif.buffer], {type: 'image/gif'}))
+          setGif(gifUrl)
+          setConvertLoading(false)
+        }
+    
+    }
+    return (
+        <div>
+            <button onClick={convert} className='convertButton'>Convert</button>
+        </div>
+    )
+}
+
+export default ConvertButton
