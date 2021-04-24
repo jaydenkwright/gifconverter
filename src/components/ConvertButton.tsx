2a84bb3406a4e1bdab0cbe8b90383a8c0485f6ed
Jayden Wright <jaydenkwright3@gmail.com>
2021-04-24 16:03:21 -0400

    Passed setError function as a prop

--- src/components/ConvertButton.tsx
+++ src/components/ConvertButton.tsx
@@ -6,18 +6,23 @@ interface Props{
     setConvertLoading: any
     setGif: any
     ffmpeg: any
+    setError: any
 }
 
-const ConvertButton: React.FC<Props> = ({ video, setConvertLoading, setGif, ffmpeg }) => {
+const ConvertButton: React.FC<Props> = ({ video, setConvertLoading, setGif, ffmpeg, setError }) => {
     const convert = async () => {
-        if (video){
-          setConvertLoading(true)
-          ffmpeg.FS('writeFile', video.name, await fetchFile(video))
-          await ffmpeg.run('-i', video.name, '-t', '30', '-f', 'gif', 'gif.gif')
-          const convertedGif = ffmpeg.FS('readFile', 'gif.gif')
-          const gifUrl = URL.createObjectURL(new Blob([convertedGif.buffer], {type: 'image/gif'}))
-          setGif(gifUrl)
-          setConvertLoading(false)
+        try{
+            if (video){
+                setConvertLoading(true)
+                ffmpeg.FS('writeFile', video.name, await fetchFile(video))
+                await ffmpeg.run('-i', video.name, '-t', '30', '-f', 'gif', 'gif.gif')
+                const convertedGif = ffmpeg.FS('readFile', 'gif.gif')
+                const gifUrl = URL.createObjectURL(new Blob([convertedGif.buffer], {type: 'image/gif'}))
+                setGif(gifUrl)
+                setConvertLoading(false)
+              }
+        }catch(e){
+            setError('Something went wrong!')
         }
     
     }
